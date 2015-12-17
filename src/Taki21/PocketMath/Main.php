caf5c7f571e8474da9a01374c774cec946c6cd89
Taki21 <shakman21@gmail.com>
2015-12-17 13:23:59 -0800

    Added ADDITION Only For now!

--- src/Taki21/PocketMath/Main.php
+++ src/Taki21/PocketMath/Main.php
@@ -8,3 +8,24 @@ use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as C;
 
 class Main extends PluginBase{
+  
+  public function onEnable{
+    $this->getLogger()->info(C::GREEN."PocketMath Enabled!");
+  }
+  public function onCommand(CommandSender $s, Command $cmd, $label, array $args){
+    if(strtolower($cmd->getName() == "add")){
+      if(!isset($args[0] or $args[1])){
+        $s->sendMessage(C::RED."Usage: /add <Number 1> <Number 2>");
+      }else{
+        if(!is_numeric($args[0] or $args[1])){
+          $s->sendMessage(C::RED."Usage: /add <Number 1> <Number 2>")
+        }else{
+          $a = array_shift($args);
+          $b = array_shift($args);
+          $c = $a + $b;
+          $s->sendMessage(C::GREEN."The Answer is $c");
+        }
+      }
+    }
+  }
+}
