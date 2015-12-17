38a0c8866a6574ef162f8568e9aa60010d6f7f49
Taki21 <shakman21@gmail.com>
2015-12-17 13:05:16 -0800

    Create Main.php

---
+++ src/Taki21/PocketMath/Main.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Taki21/PocketMath;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as C;
+
+class Main extends PluginBase{
