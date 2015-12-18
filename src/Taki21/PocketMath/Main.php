5e0034880a3cbbfec8715cc3db0f56ae8b932308
Taki21 <shakman21@gmail.com>
2015-12-18 16:26:17 -0500

    Im Done @Taki21: You code and make too much mistakes xD (FIXED)

--- src/Taki21/PocketMath/Main.php
+++ src/Taki21/PocketMath/Main.php
@@ -14,63 +14,45 @@ class Main extends PluginBase{
     }
     public function onCommand(CommandSender $s, Command $cmd, $label, array $args){
         if(strtolower($cmd->getName() == "add")){
-            if(!isset($args[0] or $args[1])){
-            $s->sendMessage(C::RED."Usage: /add <Number 1> <Number 2>");
+            if(!count($args == 0 or 1)){
+            $s->sendMessage(C::RED."Usage: /add <Number1> <Number2>");
         }else{
-            if(!is_numeric($args[0] or $args[1])){
-                $s->sendMessage(C::RED."Usage: /add <Number 1> <Number 2>");
+            $a = array_shift($args);
+            $b = array_shift($args);
+            $c = $a + $b;
+            $s->sendMessage(C::GREEN."The Answer is $c");
+          }
+        }
+        if(strtolower($cmd->getName() == "sub")){
+            if(!count($args == 0 or 1)){
+                $s->sendMessage(C::RED."Usage: /sub <Number 1> <Number 2>");
              }else{
-                $a = array_shift($args);
-                $b = array_shift($args);
-                $c = $a + $b;
-                $s->sendMessage(C::GREEN."The Answer is $c");
-                
-                    if(strtolower($cmd->getName() == "sub")){
-                        if(!isset($args[0] or $args[1])){
-                            $s->sendMessage(C::RED."Usage: /sub <Number 1> <Number 2>");
-                        }else{
-                            if(!is_numeric($args[0] or $args[1])){
-                                $s->sendMessage(C::RED."Usage: /sub <Number 1> <Number 2>");
-                            }else{
-                                $d = array_shift($args);
-                                $e = array_shift($args);
-                                $f = $d - $e;
-                                $s->sendMessage(C::GREEN."The Answer is $f");
-                                
-                                if(strtolower($cmd->getName() == "mult")){
-                                    if(!isset($args[0] or $args[1])){
-                                        $s->sendMessage(C::RED."Usage: /mult <Number 1> <Number 2>");
-                                    }else{
-                                        if(!is_numeric($args[0] or $args[1])){
-                                             $s->sendMessage(C::RED."Usage: /mult <Number 1> <Number 2>");
-                                        }else{
-                                             $g = array_shift($args);
-                                             $h = array_shift($args);
-                                             $i = $g * $h;
-                                             $s->sendMessage(C::GREEN."The Answer is $i");
-                                             
-                                             if(strtolower($cmd->getName() == "div")){
-                                                 if(!isset($args[0] or $args[1])){
-                                                    $s->sendMessage(C::RED."Usage: /div <Number 1> <Number 2>");
-                                               }else{
-                                                    if(!is_numeric($args[0] or $args[1])){
-                                                        $s->sendMessage(C::RED."Usage: /div <Number 1> <Number 2>");
-                                                    }else{
-                                                        $j = array_shift($args);
-                                                        $k = array_shift($args);
-                                                        $l = $j / $k;
-                                                        $s->sendMessage(C::GREEN."The Answer is $l");
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                   $d = array_shift($args);
+                   $e = array_shift($args);
+                   $f = $d - $e;
+                   $s->sendMessage(C::GREEN."The Answer is $f");
             }
         }
+        if(strtolower($cmd->getName() == "mult")){
+            if(!count($args == 0 or 1)){
+                $s->sendMessage(C::RED."Usage: /mult <Number 1> <Number 2>");
+            }else{
+                   $g = array_shift($args);
+                   $h = array_shift($args);
+                   $i = $g * $h;
+                   $s->sendMessage(C::GREEN."The Answer is $i");
+            }
+        }
+        if(strtolower($cmd->getName() == "div")){
+           if(!count($args == 0 or 1)){
+              $s->sendMessage(C::RED."Usage: /div <Number 1> <Number 2>");
+           }else{
+                 $j = array_shift($args);
+                 $k = array_shift($args);
+                 $l = $j / $k;
+                 $s->sendMessage(C::GREEN."The Answer is $l");
+             }
+        }
+        return true;
     }
 }
